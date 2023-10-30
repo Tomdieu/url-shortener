a293c02e164b1a23a6f7def741f5dc2052968c6a
Tomdieu <ivantomdio@gmail.com>
2023-10-30 21:49:31 +0100

    feat : added link detail route

---
+++ app/api/links/[id]/detail/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest,NextResponse } from "next/server";
+import {getLinkChartDetail} from "@/lib/getLinkChartDetail";
+
+export async function GET(req:NextRequest,{params}:{params:Record<string,string>}){
+    const short = params.id;
+    const url = new URL(req.url);
+    const {searchParams} = url
+
+    const filterBy = searchParams.get('filterBy') as "day" | "month" | "year" || "day";
+    const startDate = searchParams.get('startDate') ? new Date() : undefined;
+    const endDate = searchParams.get('endDate') ? new Date() : undefined    ;
+
+
+
+    const chartLink = await getLinkChartDetail(short,filterBy,startDate,endDate);
+    console.log("Chart Link : ",chartLink)
+    return NextResponse.json(chartLink);
+
+}
