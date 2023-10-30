16f7690de56a7a767bb39686bf00ce08387d8630
Tomdieu <ivantomdio@gmail.com>
2023-10-30 21:50:08 +0100

    feat : updated dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,6 +2,7 @@ import NavBar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
 import getCurrentUser from "@/lib/getCurrentUser";
 import { Metadata } from "next";
+import React from "react";
 
 export const metadata: Metadata = {
   title: "Trix Url | Dashboard",
@@ -17,13 +18,13 @@ export default async function DashboardLayout({
 
   return (
     <div className="h-screen w-screen flex overflow-hidden font-poppins">
-      <div className="hidden h-screen sm:w-4/12 lg:w-2/12 bg-gray-500 sm:flex">
+      <div className="hidden h-screen sm:w-4/12 md:w-4/12 xl:w-2/12 transition ease-linear bg-gray-500 sm:flex dark:border-r-gray-300 dark:border-r-1 dark:border-r-white/5">
         <Sidebar />
       </div>
-      <div className="flex flex-1 sm:w-8/12 lg:w-10/12 overflow-y-auto">
-        <div className="w-12 rounded-md flex flex-col flex-1">
+      <div className="flex flex-1 w-full sm:w-8/12 md:w-8/12 xl:w-10/12 transition ease-linear overflow-hidden">
+        <div className="w-full rounded-none flex flex-col flex-1 overflow-y-auto dark:bg-[#18181B]">
           <NavBar user={currentUser} />
-          <div className="flex-1 p-3">{children}</div>
+          <div className="flex-1 px-3 w-full dark:bg-[#18181B]">{children}</div>
         </div>
       </div>
     </div>
