9d907548aed605ac2a5c470b1a8227338067ed85
Tomdieu <ivantomdio@gmail.com>
2023-10-30 21:50:59 +0100

    updated

--- app/dashboard/links/page.tsx
+++ app/dashboard/links/page.tsx
@@ -1,25 +1,47 @@
+"use client"
 import LinkTables from "@/components/Link/LinkTables";
-import getCurrentUser from "@/lib/getCurrentUser";
 import React from "react";
-import prisma from "@/lib/prismadb";
+import {useQuery} from "@tanstack/react-query"
+import {Input,Button} from "@nextui-org/react";
+import Link from "next/link";
+import {PlusIcon} from "lucide-react";
+// import {Button} from "@/components/ui/button";
+import {Skeleton} from "@/components/ui/skeleton";
 
 
 type Props = {};
 
-const fetchUsersLink = async () => {
-  const user = await getCurrentUser();
-  const links = await prisma.link.findMany({ where: { ownerId: user?.id },include:{clicks:true} });
+export default function Links(props: Props) {
 
-  return links || [];
-}
+    const {error, isPending, isFetched, data, isLoading} = useQuery({
+        queryFn: async () => {
+            const res = await fetch(`/api/links`);
+            return (await res.json() )
+        }, queryKey: [`links`]
+    });
 
-export default async function Links(props: Props) {
 
-  const links = await fetchUsersLink();
 
-  return <div className="flex-1 flex flex-col h-full px-2 py-3">
-    <h5 className="text-2xl font-bold">Links</h5>
-    <LinkTables links={links} />
-  </div>;
+
+    return <div className="flex-1 flex flex-col h-full px-2 py-3">
+        <h5 className="text-2xl font-bold flex py-2 items-center justify-between">
+            <div className={"flex gap-5 items-center"}>
+                <h1>Links</h1>
+                <div>
+                    <input type={"search"} placeholder={"search..."} className={"border border-black px-1.5 py-2 focus:ring-indigo-500 focus:border-none border-none  block w-full shadow-sm sm:text-sm dark:border-gray-300 rounded-md"}/>
+                </div>
+            </div>
+            <Link href={"/dashboard/shorten"}>
+                <Button isIconOnly={true} className={"rounded-md"} title={"Add link"}><PlusIcon/></Button>
+            </Link>
+        </h5>
+        {isLoading && <div>
+        <Skeleton className={"h-5 w-full"}/>
+            <Skeleton className={"h-24 w-full"}/>
+
+        </div>}
+        {isFetched && !data && <div>You don&apos;t have links</div>}
+        {data && <LinkTables links={data}/>}
+    </div>;
 };
 
