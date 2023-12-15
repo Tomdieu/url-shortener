11a0b51c2aad626d6f4a6be0e811d2508ed5d79e
Tomdieu <ivantomdio@gmail.com>
2023-12-15 23:54:19 +0100

    feat : updated header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { Button } from '@nextui-org/react'
 import React,{useEffect, useState} from 'react'
 import Link from "next/link"
 import Image from 'next/image'
@@ -7,27 +6,39 @@ import { User } from '@prisma/client'
 
 import UserNav from '../navbar/user-nav'
 
+import {Button} from "@/components/ui/button"
+
 type HeaderProps = {
     user?:User|null
 }
 
 const Header = ({user}: HeaderProps) => {
     return (
-        <div className="w-full sticky top-0 left-0 right-0 z-50 px-4 flex items-center justify-between bg-white bg-opacity-40 backdrop-blur-md border-b">
+        <div className="w-full sticky top-0 left-0 right-0 z-50 px-4 flex items-center justify-between bg-white bg-opacity-40 backdrop-blur-md rounded-none border-b">
+            <div className="container flex items-center justify-between">
             <div>
             <Image src={"/logo.png"} width={500} height={500} alt='Trix Url' className='w-20 h-20' />
-                {/* <span className='font-black text-3xl font-mono'><span className="text-blue-600">T</span>rix <span className='text-orange-400'>U</span>rl</span> */}
             </div>
-            {Boolean(!user) && (<div className="flex item-center gap-2">
-                <Link href={"/auth/login"}>
-                    <Button className="rounded-full text-white bg-blue-600 hover:bg-blue-300">Login</Button>
+            <div className={"items-center flex justify-end space-x-2"}>
+                <Link href={"/dashboard"}>
+                <Button variant="link">
+                    Dashboard
+                </Button>
                 </Link>
-                <Link href={"/auth/register"}>
-                    <Button className="rounded-full text-white bg-black/80 hover:bg-black/70">Register</Button>
-                </Link>
-            </div>)}
+                {Boolean(!user) && (<div className="flex item-center gap-2">
+                    <Link href={"/auth/login"}>
+                        <Button className="rounded-full text-white bg-blue-600 hover:bg-blue-300">Login</Button>
+                    </Link>
+                    <Link href={"/auth/register"}>
+                        <Button className="rounded-full text-white bg-black/80 hover:bg-black/70">Register</Button>
+                    </Link>
+                </div>)}
 
-            {user && <UserNav/>}
+                {user && <UserNav/>}
+            </div>
+                </div>
+
+
             
         </div>
 
