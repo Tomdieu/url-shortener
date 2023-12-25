c4a6873198039b8894b51110b763108b296e5642
Tomdieu <ivantomdio@gmail.com>
2023-12-25 01:03:33 +0100

    feat : updated added Key feature section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import Hero from "@/components/Hero/Hero";
+import KeyFeatures from "@/components/KeyFeatures";
+import { Separator } from "@/components/ui/separator";
+
 import getCurrentUser from '@/lib/getCurrentUser'
 export default async function Home() {
   const user = await getCurrentUser();
@@ -8,6 +11,8 @@ export default async function Home() {
     <div className="bg-white min-h-screen flex flex-col">
       <Header user={user}/>
       <Hero/>
+      <Separator/>
+      <KeyFeatures/>
       <Footer/>
     </div>
   )
