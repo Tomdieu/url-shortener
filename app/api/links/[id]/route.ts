5f320c30b77fd8043dcc410b831922c544840cd0
Tomdieu <ivantomdio@gmail.com>
2023-12-25 01:14:02 +0100

    updated

--- app/api/links/[id]/route.ts
+++ app/api/links/[id]/route.ts
@@ -1,21 +1,49 @@
 import getCurrentUser from "@/lib/getCurrentUser";
 import { linkSchema } from "@/schema/link.schema";
-import { NextRequest,NextResponse } from "next/server";
+import { NextRequest, NextResponse, userAgent } from "next/server";
 import prisma from "@/lib/prismadb";
+import getGeolocation from "@/lib/actions/getGeolocation";
+
+function extractHostIfValidURL(str:string) {
+  const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+
+  if (urlRegex.test(str)) {
+    const url = new URL(str);
+    return url.host;
+  }
+
+  return str;
+}
+
 export async function GET(req: NextRequest, { params }: { params: Record<string, string> }) {
   const id = params.id;
-  console.log(req.ip,req.geo)
+  console.log(req.ip, req.geo)
   try {
     const user = await getCurrentUser();
-    const ipAddress = req.ip;
-    // const referringUrl = req.headers.get('Referrer') || 'Direct Visit';
+    const { searchParams } = new URL(req.url);
+
+    const deviceType = searchParams.get('deviceType')
+
     const link = await prisma.link.findFirst({ where: { short: id } });
+
+    const referrer = extractHostIfValidURL(req.headers.get("Referer") || searchParams.get("Referrer") as string || 'Direct');
+
+    const { device, os, browser } = userAgent(req)
+
+    console.log(device)
+
+    const ipAddress = req.headers.get('x-forwarded-for');
+
+    console.log("Ip Address : ", req.headers.get('x-forwarded-for'))
+
+    const country = await getGeolocation(ipAddress!, process.env.API_INFO_TOKEN!)
+
     // we are going to increment the link click if if exists and the owner of that url is not the one accessing it
     if (link) {
 
       if (!user || (user?.id !== link.ownerId)) {
-  
-        await prisma.click.create({ data: { linkId: link.id, ipAddress, } });
+
+        await prisma.click.create({ data: { linkId: link.id, ipAddress, country, referrer, device:deviceType || device.type, os: os.name, browser: browser?.name } });
       }
     }
     return NextResponse.json(link, { status: 200 });
