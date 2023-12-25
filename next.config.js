0c417cdc80def40b8344911abffe848fdd71b1d3
Tomdieu <ivantomdio@gmail.com>
2023-12-25 01:06:10 +0100

    feat : updated images key

--- next.config.js
+++ next.config.js
@@ -1,14 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            "lh3.googleusercontent.com",
-            "avatars.githubusercontent.com",
-            "ui.shadcn.com",
-            "*"
-        ],
+        // domains: [
+        //     "lh3.googleusercontent.com",
+        //     "avatars.githubusercontent.com",
+        //     "ui.shadcn.com",
+        //     "*.*"
+        // ],
+        remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'lh3.googleusercontent.com',
+              pathname: '**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+                pathname: '**',
+              },
+              {
+                protocol: 'https',
+                hostname: 'ui.shadcn.com',
+                pathname: '**',
+              },
+              {
+                protocol: 'https',
+                hostname: '**.*',
+              },
+          ],
     },
-    experimental: {},
+    experimental: {
+      ppr: true,
+    },
 };
 
 module.exports = nextConfig;
