54141e9b161b820d155d2e05d7b8a80c93e816fc
Tomdieu <ivantomdio@gmail.com>
2023-12-26 09:09:57 +0100

    feat : updated nivo calendar to react-calendar-heatmap

--- components/nivo/Calendar.tsx
+++ components/nivo/Calendar.tsx
@@ -3,6 +3,9 @@
 import React from 'react';
 import { ResponsiveCalendar } from '@nivo/calendar';
 
+import CalendarHeatmap from 'react-calendar-heatmap';
+import 'react-calendar-heatmap/dist/styles.css';
+
 export type nivoCalendarType = {
     day: string;
     value: number;
@@ -22,21 +25,47 @@ export default function Calendar({ data, className, style }: CalendarProps) {
     const currentYear = currentDate.getFullYear();
     const previousYear = currentYear - 1;
 
+    // console.log(data)
+
     const filteredData = data.filter((entry) => {
         const entryYear = new Date(entry.day).getFullYear();
         return entryYear === currentYear || entryYear === previousYear;
-      });
+    });
+    const weekdayLabels = ['Sun ', 'Mon ', 'Tue ', 'Wed ', 'Thu ', 'Fri ', 'Sat '];
+    console.log(`${currentYear}-${currentDate.getMonth() -10}-${currentDate.getDate()}`," - ",`${currentYear}-${currentDate.getMonth()+1}-${currentDate.getDate()}`)
     return (
-        <div style={{ height: '400px', width: '100%', ...style }} className={className}>
-            <ResponsiveCalendar
+        <div className={className} style={{...style }}>
+            <CalendarHeatmap 
+                startDate={`${currentYear - 1}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`}
+                endDate={currentDate}
+                values={filteredData.map((entry) => ({
+                    date: entry.day,
+                    count: entry.value,
+                }))}
+                showWeekdayLabels={true}
+                classForValue={(value) => {
+                    if (!value) {
+                      return 'color-empty';
+                    }
+                    return `color-scale-${value.count}`;
+                  }}
+                  tooltipDataAttrs={(value) => {
+                    return {
+                      'data-tip': `${value.date} has count: ${value.count}`,
+                    };
+                  }}
+                
+                weekdayLabels={weekdayLabels}
+            />
+            {/* <ResponsiveCalendar
                 data={filteredData.map((entry) => ({
                     day: entry.day,
                     value: entry.value,
-                  }))}
-                // from={from}
-                from={`${currentYear - 1}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`}
-                to={`${currentYear}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`}
+                }))}
                 
+                from={`${currentYear}-${currentDate.getMonth() -10}-${currentDate.getDate()}`}
+                to={`${currentYear}-${currentDate.getMonth()+1}-${currentDate.getDate()}`}
+
                 emptyColor="#eeeeee"
                 colors={colorScale}
                 margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
@@ -57,7 +86,8 @@ export default function Calendar({ data, className, style }: CalendarProps) {
                         itemDirection: 'right-to-left',
                     },
                 ]}
-            />
+            /> */}
         </div>
+
     )
 }
