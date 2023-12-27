c5050f5e4974ae733c21553563cbcca8c84fba04
Tomdieu <ivantomdio@gmail.com>
2023-12-27 23:08:45 +0100

    feat : updated schema

---
+++ schema/user.schema.ts
@@ -0,0 +1,19 @@
+import {z} from "zod"
+
+export const userSchema = z.object({
+    name:z.string(),
+    email:z.string(),
+    password:z.string().optional(),
+    confirmPassword:z.string().optional()
+}).refine(
+    (values) => {
+        return values.password === values.confirmPassword;
+    },
+    {
+        message: "Passwords must match!",
+        path: ["confirmPassword"],
+    }
+);
+
+
+export type UserType = z.infer<typeof userSchema>
