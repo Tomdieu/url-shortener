1c22d15dcaa3a6709c387feb13910b1243d55fb2
Tomdieu <ivantomdio@gmail.com>
2023-12-27 23:30:13 +0100

    feat : update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import NextUiProvider from "@/providers/NextUiProvider";
 import "./globals.css";
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import NextTopLoader from "nextjs-toploader";
-import { Toaster } from "react-hot-toast";
+import {Toaster} from "react-hot-toast";
 import localFont from 'next/font/local'
 import React from "react";
 import NextThemeProvider from "@/providers/NextThemeProvider";
@@ -29,7 +29,7 @@ const poppins = localFont({
 export const metadata: Metadata = {
     title: "URL Shortener | Trix Url",
     description: "Trix Url is a web app use to shortened long Url",
-    authors: [{ name: "Tomdieu Ivan", url: "https://github.com/tomdieu" }],
+    authors: [{name: "Tomdieu Ivan", url: "https://github.com/tomdieu"}],
     creator: "Tomdieu Ivan",
     metadataBase: new URL('https://trixurl.vercel.app'),
     robots: {
@@ -46,9 +46,9 @@ export const metadata: Metadata = {
         },
     },
     category: "URL Shortener",
-    icons: [{ rel: "icon", url: "https://trixurl.vercel.app/icon.png" },
+    icons: [{rel: "icon", url: "https://trixurl.vercel.app/icon.png"},
 
-    { rel: "apple-touch-icon", url: "https://trixurl.vercel.app/logo.png" }],
+        {rel: "apple-touch-icon", url: "https://trixurl.vercel.app/logo.png"}],
     twitter: {
         card: 'summary_large_image',
         title: 'TrixUrl',
@@ -63,38 +63,41 @@ export const metadata: Metadata = {
         creators: ['@tomdieu ivan'],
         url: 'https://trixurl.vercel.app',
 
+    },
+    verification:{
+        google:"9sayQN2cMoBQa3PuAWCdz2IBCRMn8iy6zU63_Pq6sHg"
     }
 };
 
 export const viewport = {
     themeColor: [
-      { media: '(prefers-color-scheme: light)', color: 'cyan' },
-      { media: '(prefers-color-scheme: dark)', color: 'black' },
+        {media: '(prefers-color-scheme: light)', color: 'cyan'},
+        {media: '(prefers-color-scheme: dark)', color: 'black'},
     ],
     width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
-  }
+    initialScale: 1,
+    maximumScale: 1,
+}
 
 export default async function RootLayout({
-    children,
-}: {
+                                             children,
+                                         }: {
     children: React.ReactNode;
 }) {
     return (
         <html lang="en" className={`${poppins.variable}`} suppressHydrationWarning={true}>
 
-            <body>
-                <Toaster />
-                <NextTopLoader showSpinner={false} />
-                <NextThemeProvider>
-                    <NextUiProvider>
-                        <ReactQueryProvider>
-                            {children}
-                        </ReactQueryProvider>
-                    </NextUiProvider>
-                </NextThemeProvider>
-            </body>
+        <body>
+        <Toaster/>
+        <NextTopLoader showSpinner={false}/>
+        <NextThemeProvider>
+            <NextUiProvider>
+                <ReactQueryProvider>
+                    {children}
+                </ReactQueryProvider>
+            </NextUiProvider>
+        </NextThemeProvider>
+        </body>
 
         </html>
     );
