cf4e7e20f3ffd9618b02353feeb7dc946026428a
Tomdieu <ivantomdio@gmail.com>
2023-12-27 23:27:29 +0100

    feat : updated react-query-provider

--- providers/ReactQueryProvider.tsx
+++ providers/ReactQueryProvider.tsx
@@ -16,7 +16,7 @@ export default function ReactQueryProvider({children}:ReactQueryProviderProps){
     return (
         <QueryClientProvider client={queryClient}>
             {children}
-            <ReactQueryDevtools initialIsOpen={false} />
+            {/*<ReactQueryDevtools initialIsOpen={false} />*/}
         </QueryClientProvider>
     );
 }
