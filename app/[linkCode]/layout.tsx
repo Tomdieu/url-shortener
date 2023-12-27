9a18dda2478b27d00fe2f1084309a56614224ae5
Tomdieu <ivantomdio@gmail.com>
2023-12-27 23:56:57 +0100

    fix

--- app/[linkCode]/layout.tsx
+++ app/[linkCode]/layout.tsx
@@ -5,23 +5,16 @@ import {previewLink} from "@/lib/actions/preview-link"
 
 type Props = {
     params: { linkCode: string }
-    searchParams: { [key: string]: string | string[] | undefined }
 }
 
 export async function generateMetadata(
-    { params, searchParams }: Props,
+    { params }: Props,
     parent: ResolvingMetadata
 ): Promise<Metadata> {
     const linkCode = params.linkCode
 
     const link = await getLink(linkCode)
 
-    if(!link.success){
-        return {
-            title:linkCode + " : Not Found",
-            description:"No Description For this link"
-        }
-    }
 
     // optionally access and extend (rather than replace) parent metadata
     const previousImages = (await parent).openGraph?.images || []
@@ -36,21 +29,21 @@ export async function generateMetadata(
         title: process.env.URL+"/"+linkCode,
         description:"Total Clicks : "+link.data?.clicks.length,
         openGraph: {
-            images: [image, ...previousImages],
+            images: [image,process.env.URL+'/icon.png', ...previousImages],
             description:"Total Clicks : "+link.data?.clicks.length,
             title:original,
             url:process.env.URL,
             tags:["Trix Url",original],
         },
         twitter:{
-            images: [image, ...previousImages],
+            images: [image,process.env.URL+'/icon.png', ...previousImages],
             description:"Total Clicks : "+link.data?.clicks.length,
             title:original,
         }
     }
 }
 
-export default async function Layout({children}:{children:React.ReactNode}){
+export default function Layout({children}:{children:React.ReactNode}){
     return (
         <div className={"flex flex-1 w-full h-full"}>
             {children}
