26869d6abf6240620f9d1edfb9c7f5403eaa2e7d
Tomdieu <ivantomdio@gmail.com>
2024-11-08 17:59:57 +0100

    feat : added jsonld structure

---
+++ constants/jsonld.ts
@@ -0,0 +1,100 @@
+export const JSONLD = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    "name": {
+      "@language": "en",
+      "@value": "Trix URL - Your Ultimate URL Shortener"
+    },
+    "url": "https://trixurl.vercel.app",
+    "description": {
+      "@language": "en",
+      "@value": "Shorten and manage URLs with ease using Trix URL, the ultimate link shortening tool. Track analytics, customize links, and simplify your online experience."
+    },
+    "keywords": [
+      "Trix URL",
+      "URL shortener",
+      "link management",
+      "link tracking",
+      "analytics",
+      "custom links"
+    ],
+    "inLanguage": "en",
+    "creator": {
+      "@type": "Person",
+      "name": "Tomdieu Ivan",
+      "url": "https://github.com/tomdieu"
+    },
+    "author": {
+      "@type": "Person",
+      "name": "Tomdieu Ivan",
+      "url": "https://github.com/tomdieu"
+    },
+    "publisher": {
+      "@type": "Organization",
+      "name": "Trix URL",
+      "logo": {
+        "@type": "ImageObject",
+        "url": "https://trixurl.vercel.app/icon.png",
+        "width": 512,
+        "height": 512
+      }
+    },
+    "potentialAction": {
+      "@type": "SearchAction",
+      "target": "https://trixurl.vercel.app/?q={search_term_string}",
+      "query-input": "required name=search_term_string"
+    },
+    "image": {
+      "@type": "ImageObject",
+      "url": "https://trixurl.vercel.app/icon.png",
+      "width": 1200,
+      "height": 630,
+      "caption": "Trix URL - Your Ultimate URL Shortener"
+    },
+    "sameAs": [
+      "https://github.com/tomdieu",
+      "https://twitter.com/tomdieuivan"
+    ],
+    "mainEntityOfPage": {
+      "@type": "WebPage",
+      "@id": "https://trixurl.vercel.app"
+    },
+    "identifier": {
+      "@type": "PropertyValue",
+      "name": "Trix URL",
+      "value": "https://trixurl.vercel.app"
+    },
+    "twitter": {
+      "@type": "SocialMediaPosting",
+      "accountId": "@tomdieuivan",
+      "creator": {
+        "@type": "Person",
+        "name": "Tomdieu Ivan"
+      },
+      "datePublished": "2024-11-08"
+    },
+    "robots": {
+      "index": true,
+      "follow": true,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+      "max-video-preview": -1
+    },
+    "description_fr": {
+      "@language": "fr",
+      "@value": "Raccourcissez et gérez vos URL facilement avec Trix URL, l'outil ultime de raccourcissement de liens. Suivez les analyses, personnalisez vos liens et simplifiez votre expérience en ligne."
+    },
+    "keywords_fr": [
+      "Trix URL",
+      "raccourcisseur d'URL",
+      "gestion de liens",
+      "suivi de liens",
+      "analytique",
+      "liens personnalisés"
+    ],
+    "name_fr": {
+      "@language": "fr",
+      "@value": "Trix URL - Le Raccourcisseur d'URL Ultime"
+    }
+  }
+  
